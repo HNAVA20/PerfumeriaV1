8f8c63f1364898713b868722bac68c8987da9ded
Kelvim Isahí Gutiérrez Sandoval <kelvimisahigutierrezsandoval@MacBook-Air-de-Kelvim.local>
2025-02-24 13:45:59 -0700

    unisex y sets

---
+++ src/pages/unisex.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import '../styles/unisex.css'
+
+function unisex() {
+    return(
+        <h1>Sección Unisex</h1>
+    );
+}
+
+export default unisex;
