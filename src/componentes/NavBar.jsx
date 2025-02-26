a18e479a475261864a842eeb0587a504d7ac179f
Kelvim Isahí Gutiérrez Sandoval <kelvimisahigutierrezsandoval@MacBook-Air-de-Kelvim.local>
2025-02-25 23:29:07 -0700

    Menu de 3 niveles

--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -29,10 +29,26 @@ const Navbar = () => {
           <li><Link to="/">Inicio</Link></li>
 
           <li className={`submenu ${submenuOpen === 1 ? "open" : ""}`}>
-            <a href="#" onClick={() => toggleSubmenu(1)}>Perfumes</a>
+            <Link to="/perfumes" onClick={() => toggleSubmenu(1)}>Perfumes</Link>
             <ul>
-              <li><Link to="/dama">Dama</Link></li>
-              <li><Link to="/caballero">Caballero</Link></li>
+              <li className={`submenu ${submenuOpen === 2 ? "open" : ""}`}>
+                <Link to="/dama" onClick={() => toggleSubmenu(2)}>Dama</Link>
+                <ul>
+                  <li><Link to="/dama/chanel">Chanel</Link></li>
+                  <li><Link to="/dama/dior">Dior</Link></li>
+                  <li><Link to="/dama/lancome">Lancôme</Link></li>
+                  <li><Link to="/dama/armani">Armani</Link></li>
+                </ul>
+              </li>
+              <li className={`submenu ${submenuOpen === 3 ? "open" : ""}`}>
+                <Link to="/caballero" onClick={() => toggleSubmenu(3)}>Caballero</Link>
+                <ul>
+                  <li><Link to="/caballero/hugo-boss">Hugo Boss</Link></li>
+                  <li><Link to="/caballero/dolce-gabbana">Dolce & Gabbana</Link></li>
+                  <li><Link to="/caballero/yves-saint-laurent">Yves Saint Laurent</Link></li>
+                  <li><Link to="/caballero/paco-rabanne">Paco Rabanne</Link></li>
+                </ul>
+              </li>
               <li><Link to="/unisex">Unisex</Link></li>
               <li><Link to="/ninos">Niños</Link></li>
               <li><Link to="/sets">Sets</Link></li>
