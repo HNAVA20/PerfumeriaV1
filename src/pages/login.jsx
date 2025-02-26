c60f5bd1bf99f8d55883789e4c7126e3c859a62e
Kelvim Isahí Gutiérrez Sandoval <kelvimisahigutierrezsandoval@MacBook-Air-de-Kelvim.local>
2025-02-25 22:28:53 -0700

    elimancion de Formulario registro

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import '../styles/login.css';
-import Form from "../componentes/FormRegistro.jsx";
 
 function Login() {
     return(
-        <Form />
+        <h1>Hello World!</h1>
     );
 }
 
