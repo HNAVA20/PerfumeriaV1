397e4c1bea51686e1987401685d8b523f488576b
Kelvim Isahí Gutiérrez Sandoval <kelvimisahigutierrezsandoval@MacBook-Air-de-Kelvim.local>
2025-03-03 13:38:49 -0700

    cambios

--- src/componentes/Breadcrumb.jsx
+++ src/componentes/Breadcrumb.jsx
@@ -1,52 +1,29 @@
 import React from "react";
-import '../componentes/Breadcrumb.css';
-import { Breadcrumbs, Link, Box, Typography} from "@material-ui/core";
-import { HomeWorkOutlined } from "@material-ui/icons";
-import { Link } from "react-router-dom";
-
-function App(){
-
-    return(
-        <Box display="Flex" flexDirection="column" m={10}>
-            <Breadcrumbs>
-            <Link className="breadcrumb-link" href="/" onClick={() => {}}>
-            Inicio
-            </Link>
-            <Link className="breadcrumb-link" href="/" onClick={() => {}}>
-            Perfumes
-            </Link>
-            <Link className="breadcrumb-link" href="/" onClick={() => {}}>
-            Novedades
-            </Link>
-            <Typography color="textPrimary">Page-1</Typography>
-            </Breadcrumbs>
-
-            <Breadcrumbs separator="->">
-            <Link className="breascrumb-link" href="/" onClick={() => {}}>
-            Pages
-            </Link>
-            <Typography color="textPrimary">Page-1</Typography>
-            </Breadcrumbs>
-
-            <Breadcrumbs>
-            <Link className="breadcrumb-link breadcrumb-icon-link" href="/" onClick={() => {}}>
-            <HomeWorkOutlined className="breadcrumb-icon" />
-            Inicio
-            </Link>
-            <Typography color="textPrimary">Page-1</Typography>
-            </Breadcrumbs>
-
-            <Breadcrumbs maxItems={2}>
-            <Typography color="textPrimary">Inicio</Typography>
-            <Typography color="textPrimary">Perfumes</Typography>
-            <Typography color="textPrimary">All pages</Typography>
-            <Typography color="textPrimary">Pages</Typography>
-            <Typography color="textPrimary">Page-1</Typography>
-
-        </Breadcrumbs>
-         </Box>
-    );
-}
+import { Link, useLocation } from "react-router-dom";
+import "../componentes/Breadcrumb.css";
 
 
-export default App;
+const Breadcrumbs = () => {
+  const location = useLocation();
+  const pathnames = location.pathname.split("/").filter((x) => x);
+
+  return (
+    <nav aria-label="breadcrumb">
+      <ul className="breadcrumbs">
+        <li>
+          <Link to="/">Inicio</Link>
+        </li>
+        {pathnames.map((value, index) => {
+          const to = `/${pathnames.slice(0, index + 1).join("/")}`;
+          return (
+            <li key={to}>
+              <Link to={to}>{decodeURIComponent(value)}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Breadcrumbs;
