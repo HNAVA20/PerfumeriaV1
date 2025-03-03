a703ee7d6298d4e4feef2e352dc8d95613fbc59a
HNAVA20 <ernestomeraz98@gmail.com>
2025-03-03 18:59:25 +0000

    Actualizacion de perfumes.css y jsx

--- src/pages/perfumes.jsx
+++ src/pages/perfumes.jsx
@@ -1,10 +1,16 @@
 import React from "react";
-import '../styles/perfumes.css';
+import "../styles/perfumes.css";
 
 function Perfumes() {
-    return(
-        <div>
-            <h1>Hello</h1>
+    const categories = ["Dama", "Caballero", "Unisex", "Niños", "Sets"];
+
+    return (
+        <div className="category-container">
+            {categories.map((category, index) => (
+                <div key={index} className="category-card">
+                    <h2>{category}</h2>
+                </div>
+            ))}
         </div>
     );
 }
