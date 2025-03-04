6c4f87466c843fb27f05b6ab04ed9d1394c2bae0
HNAVA20 <ernestomeraz98@gmail.com>
2025-03-04 18:04:31 +0000

    Actualizacion de las tarjeta de categoria en perfumes .jsx y css

--- src/pages/perfumes.jsx
+++ src/pages/perfumes.jsx
@@ -1,14 +1,33 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Importar para la navegación
 import "../styles/perfumes.css";
+import dama from "../imagenes/dama.png";
+import caballero from "../imagenes/caballero.png";
+import unisex from "../imagenes/unisex.png";
+import ninos from "../imagenes/ninos.png";
+import sets from "../imagenes/sets.png";
 
 function Perfumes() {
-    const categories = ["Dama", "Caballero", "Unisex", "Niños", "Sets"];
+    const navigate = useNavigate();
+
+    const categories = [
+        { name: "Dama", image: dama, path: "/dama" },
+        { name: "Caballero", image: caballero, path: "/caballero" },
+        { name: "Unisex", image: unisex, path: "/unisex" },
+        { name: "Niños", image: ninos, path: "/ninos" },
+        { name: "Sets", image: sets, path: "/sets" }
+    ];
 
     return (
         <div className="category-container">
             {categories.map((category, index) => (
-                <div key={index} className="category-card">
-                    <h2>{category}</h2>
+                <div 
+                    key={index} 
+                    className="category-card" 
+                    style={{ backgroundImage: `url(${category.image})` }} 
+                    onClick={() => navigate(category.path)}
+                >
+                    <div className="category-label">{category.name}</div>
                 </div>
             ))}
         </div>
