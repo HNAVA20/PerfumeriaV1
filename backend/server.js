421545c2a0c62ec3074886c4509fbff5cc2ac4e0
elgaryow <mepelanla246@gmail.com>
2025-03-24 21:35:14 -0700

    Modificado backend/server.js para resolver conflictos

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ const upload = multer({
 
 
 // Inicia el servidor en el puerto 3000
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
